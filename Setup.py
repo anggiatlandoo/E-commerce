25c44d40807d3d20a9f40070d43fa73b6a55dd57
Anggiat Lando <50763962+anggiatlandoo@users.noreply.github.com>
2024-02-28 05:26:53 +0700

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,14 @@
+```python
+   from setuptools import setup, find_packages
+
+   setup(
+       name='E-commerce',
+       version='0.1',
+       packages=find_packages(),
+       url='https://github.com/anggiatlandoo/E-commerce',
+       author='anggiatlando',
+       author_email='anggiatlandoo@gmail.com',
+       description='Ini adalah proyek open source yang mengimplementasikan sebuah platform e-commerce dengan fitur-fitur seperti otentikasi pengguna, penanganan pesanan, manajemen produk, dan lainnya:,
+       
+   )
+   ```
